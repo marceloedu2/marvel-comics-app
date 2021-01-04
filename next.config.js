1ecb0b1c837fed3600d32de85c678ffdbf00a6ed
Marcelo Araujo <marcelo.edu2@gmail.com>
2021-01-04 11:47:33 -0300

    configurando .env

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-
-const isProd = process.env.NODE_END === 'production'
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  }
-})
-
 module.exports = {
   plugins: [['module-resolver', { root: ['./src'], alias: { '@': './src' } }]]
 }
